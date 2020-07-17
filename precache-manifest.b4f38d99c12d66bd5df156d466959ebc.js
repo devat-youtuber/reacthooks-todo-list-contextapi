9f467bb5129a36c852a5d3c0c90e738cf5a0274b
dev at <battuosai01@gmail.com>
2020-07-17 11:51:03 +0700

    Updates

---
+++ precache-manifest.b4f38d99c12d66bd5df156d466959ebc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebebdf2bb9b1c9c5b8e7f3304d8ee3c",
+    "url": "/reacthooks-todo-list-contextapi/index.html"
+  },
+  {
+    "revision": "6dee8a9d017aa0ffbe9a",
+    "url": "/reacthooks-todo-list-contextapi/static/css/main.af5d0e1a.chunk.css"
+  },
+  {
+    "revision": "13b513e79a21f990740c",
+    "url": "/reacthooks-todo-list-contextapi/static/js/2.5f3d3106.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacthooks-todo-list-contextapi/static/js/2.5f3d3106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dee8a9d017aa0ffbe9a",
+    "url": "/reacthooks-todo-list-contextapi/static/js/main.077bf151.chunk.js"
+  },
+  {
+    "revision": "4783e98d8b0ffb726b5f",
+    "url": "/reacthooks-todo-list-contextapi/static/js/runtime-main.223535c4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reacthooks-todo-list-contextapi/precache-manifest.b4f38d99c12d66bd5df156d466959ebc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reacthooks-todo-list-contextapi/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
